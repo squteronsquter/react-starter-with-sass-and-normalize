7f4de9dcda4f7dbfd629198200845ffee0509bd5
squteronsquter <squter@squter.pl>
2020-01-14 10:28:29 +0100

    Initial State with sass support  and Normalize.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<header className='heading'>
+				<h1 className='heading--title'>Horeca Bay Under Construction.</h1>
+				<h2 className='heading--subtitle'>
+					This website is under development.
+				</h2>
+				<p className='heading--text'>
+					Please revisit in a couple of weeks to experience the new website in
+					full.
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
